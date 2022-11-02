284d0366110c59be3995533c44153f56b430eab8
saicem <saicem@qq.com>
2022-11-02 13:23:03 +0800

    fix: add NameError

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import urllib3.util
 import uvicorn
 from fastapi import FastAPI
 from playwright.async_api import async_playwright
-from starlette.responses import StreamingResponse, PlainTextResponse
+from starlette.responses import StreamingResponse, PlainTextResponse, Response
 
 app = FastAPI()
 
@@ -15,18 +15,22 @@ async def playwright_screenshot(url: str, width: int = 390, height: int = 844):
         browser = await p.chromium.launch()
         ctx = await browser.new_context(ignore_https_errors=True, viewport={'width': width, 'height': height})
         page = await ctx.new_page()
+        res: Response
         try:
             page.set_default_timeout(10000)
             await page.goto(str(urllib3.util.parse_url(url)))
             screenshot_bytes = await page.screenshot()
+            res = StreamingResponse(content=io.BytesIO(screenshot_bytes), media_type="image/png")
         except TimeoutError:
-            return PlainTextResponse("请求超时", status_code=400)
-        await page.close()
-        await browser.close()
-        return StreamingResponse(content=io.BytesIO(screenshot_bytes), media_type="image/png")
+            res = PlainTextResponse("请求超时", status_code=400)
+        except NameError:
+            res = PlainTextResponse("域名错误", status_code=400)
+        finally:
+            await page.close()
+            await browser.close()
+        return res
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     uvicorn.run(
         "main:app",
